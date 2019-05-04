fb4cc0ce1aedbb09eadf2baad16c78bdeebda124
Horváth Dániel <admin@hdani1337.hu>
2019-05-04 22:33:09 +0200

    Boss random speed

--- core/src/hu/hdani1337/marancsicsDash/Actor/MarancsicsBoss.java
+++ core/src/hu/hdani1337/marancsicsDash/Actor/MarancsicsBoss.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import hu.hdani1337.marancsicsDash.Global.Assets;
 import hu.hdani1337.marancsicsDash.MyBaseClasses.Scene2D.MyRectangle;
 import hu.hdani1337.marancsicsDash.MyBaseClasses.Scene2D.OneSpriteAnimatedActor;
+import hu.hdani1337.marancsicsDash.Stage.OptionsStage;
 
 public class MarancsicsBoss extends OneSpriteAnimatedActor {
 
     public static float marancsicsHealth = 99.9f;
+    private byte speed;
     public Viewport tempView;
 
     public MarancsicsBoss(Viewport viewport) {
@@ -20,14 +22,23 @@ public class MarancsicsBoss extends OneSpriteAnimatedActor {
         tempView = viewport;
         setFps(18);
         setSize(getWidth()*1.5f,getHeight()*1.5f);
+        if(OptionsStage.difficulty == 1) speed = 4;
+        else if(OptionsStage.difficulty == 3) speed = 8;
+        else speed = 6;
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        if(marancsicsHealth < 0) marancsicsHealth = 0;
-        else if(marancsicsHealth > 99.9) marancsicsHealth = 99.9f;
-        setX(getX() - 8);
-        if(getX() + getWidth() < 0) setX(tempView.getWorldWidth());
+        if (marancsicsHealth < 0) marancsicsHealth = 0;
+        else if (marancsicsHealth > 99.9) marancsicsHealth = 99.9f;
+        setX(getX() - speed);
+        if (getX() + getWidth() < 0)
+        {
+            setX(tempView.getWorldWidth());
+            if(OptionsStage.difficulty == 1) speed = (byte)(Math.random() * 5 + 4);
+            else if(OptionsStage.difficulty == 3) speed = (byte)(Math.random() * 5 + 5);
+            else speed = (byte)(Math.random() * 5 + 7);
+        }
     }
 }
